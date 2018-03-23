77566cc00a755145fd29a70ab7152c1e8cfc8635
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2018-03-23 22:28:02 +0530

    Create Factorials!.js

---
+++ Udacity Challenges/Factorials!.js
@@ -0,0 +1,16 @@
+/*
+Write a for loop that prints out the factorial of the number 12:
+*/
+
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+var solution=1;
+for(var x=12; x>0 ; x--)
+{
+     solution *= x;
+    
+}
+console.log(solution);
