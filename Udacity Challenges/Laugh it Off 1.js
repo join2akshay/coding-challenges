862e8eef5c13d21e1115dddcc23fc0cc8a978ff0
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2018-03-30 22:29:17 +0530

    Create  Laugh it Off 1.js

---
+++ Udacity Challenges/Laugh it Off 1.js
@@ -0,0 +1,11 @@
+/*
+Declare a function called laugh() that returns "hahahahahahahahahaha!". 
+Print the value returned from the laugh() function to the console.
+
+*/
+
+
+function laugh(){
+    return "hahahahahahahahahaha!";
+}
+console.log(laugh());
