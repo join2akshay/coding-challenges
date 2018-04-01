6a1786e14c4b9d54d8f4fb5ad6b2506b1176bc2e
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2018-04-01 22:06:14 +0530

    Create UdaciFamily.js

---
+++ Udacity Challenges/UdaciFamily.js
@@ -0,0 +1,8 @@
+/*
+Create an array called udaciFamily and add "Julia", "James", and your name to the array. 
+Then, print the udaciFamily to the console using console.log.
+
+*/
+
+var udaciFamily=["Julia","James","Akshay"];
+console.log(udaciFamily);
