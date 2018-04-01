ca8be99d9409e9ee2b61835763d5ae5e8870117c
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2018-04-01 22:13:18 +0530

    Create Cry.js

---
+++ Udacity Challenges/Cry.js
@@ -0,0 +1,12 @@
+/*
+Write a named function expression that stores the function in a variable called cry and returns "boohoo!". 
+Don't forget to call the function using the variable name, not the function name:
+
+*/
+
+
+var cry= function m()
+{
+    return "boohoo!";
+}
+console.log(cry());
