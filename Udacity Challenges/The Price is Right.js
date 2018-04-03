c753981a8d33b407bf001215e0e42294f882f72e
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2018-04-03 21:17:17 +0530

    Create The Price is Right.js

---
+++ Udacity Challenges/The Price is Right.js
@@ -0,0 +1,14 @@
+/*
+Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+*/
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
+ prices[0]=1;
+ prices[2]=2;
+ prices[6]=3;
+console.log(prices);
