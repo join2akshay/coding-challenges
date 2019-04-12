73d4d191adede965884d37fe6c99d79d738eede3
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2019-04-12 21:33:08 +0530

    Create Change the Prototype to a New Object.js

---
+++ FreeCodeCamp/Javascript Algorithms And Data Structures Certification (300 hours)/Object Oriented Programming/Change the Prototype to a New Object.js
@@ -0,0 +1,18 @@
+/*
+Add the property numLegs and the two methods eat()
+and describe() to the prototype of Dog by setting the prototype to a new object.
+*/
+function Dog(name) {
+  this.name = name; 
+}
+
+Dog.prototype = {
+  // Add your code below this line
+  numLegs : 2,
+  eat : function(){
+    console.log("woff woff");
+  },
+  describe(){
+    console.log("My name is"+ this.name);
+  }
+};
