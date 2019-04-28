0acd9fa1b04c1cd2bffd8cd013e366b84bd451e8
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2019-04-28 22:33:30 +0530

    Create Use a Mixin to Add Common Behavior Between Unrelated Objects.js

---
+++ FreeCodeCamp/Javascript Algorithms And Data Structures Certification (300 hours)/Object Oriented Programming/Use a Mixin to Add Common Behavior Between Unrelated Objects.js
@@ -0,0 +1,24 @@
+/*
+Create a mixin named glideMixin that defines a method named glide.
+Then use the glideMixin to give both bird and boat the ability to glide.
+*/
+
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+// Add your code below this line
+let glideMixin = function(obj){
+    obj.glide = function(){
+        console.log('Flying, wooosh!');
+    }
+}
+
+glideMixin(bird);
+glideMixin(boat);
