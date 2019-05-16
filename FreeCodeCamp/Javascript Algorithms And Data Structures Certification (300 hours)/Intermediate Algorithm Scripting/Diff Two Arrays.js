de93d6230d61be5b3b89d7f5276dfb9caddd3840
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2019-05-16 23:34:09 +0530

    Create Diff Two Arrays.js

---
+++ FreeCodeCamp/Javascript Algorithms And Data Structures Certification (300 hours)/Intermediate Algorithm Scripting/Diff Two Arrays.js
@@ -0,0 +1,16 @@
+/*
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays
+*/
+
+function diffArray(arr1, arr2) {
+  return [
+    ...diff(arr1, arr2),
+    ...diff(arr2, arr1)
+  ]
+  
+  function diff(a, b) {
+    return a.filter(item => b.indexOf(item) === -1);
+  }
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
